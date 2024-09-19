acb33496fc65ae09c4265c5a16630d52ca87e9b0
FlorianFrei <freitaf99@zedat.fu-berlin.de>
2024-09-19 18:24:30 +0200

    new

--- OE_BPOD_to_DF/KS_BPOD_to_df.py
+++ OE_BPOD_to_DF/KS_BPOD_to_df.py
@@ -10,19 +10,17 @@ import pandas as pd
 from helpers import*
 
 #%%
-cluster_group= pd.read_csv('E:/Florian/SNA-127316_2023-12-16_14-41-01/sorted/phy/cluster_group.tsv',sep = '\t')
-clust = np.array(np.load('E:/Florian/SNA-127316_2023-12-16_14-41-01/sorted/phy/spike_clusters.npy'))
-times = np.array(np.load('E:/Florian/SNA-127316_2023-12-16_14-41-01/sorted//phy/spike_times.npy'))[:,0]
-ITI = np.load('E:/Florian/SNA-127316_2023-12-16_14-41-01/Record Node 171/experiment1/recording1/events/Intan_RHD_USB-170.Rhythm Data/TTL/timestamps.npy')
-TTL_states = np.load('E:/Florian/SNA-127316_2023-12-16_14-41-01/Record Node 171/experiment1/recording1//events/Intan_RHD_USB-170.Rhythm Data/TTL/states.npy')
-raw_BPOD = load_mat('E:/Florian/SNA-127316_2023-12-16_14-41-01/m3_MA_20231216_144158')
-sample_nums = np.load('E:/Florian/SNA-127316_2023-12-16_14-41-01/Record Node 171/experiment1/recording1/continuous/Intan_RHD_USB-170.Rhythm Data/sample_numbers.npy')
-TTL_sample_times = np.load('E:/Florian/SNA-127316_2023-12-16_14-41-01/Record Node 171/experiment1/recording1/events/Intan_RHD_USB-170.Rhythm Data/TTL/sample_numbers.npy')
+cluster_group= pd.read_csv("D:/M8_SNA-135381_17092024_1_g0_imec1/sorted/phy/cluster_group.tsv",sep = '\t')
+clust = np.array(np.load("D://M8_SNA-135381_17092024_1_g0_imec1/sorted/phy/spike_clusters.npy"))
+times = np.array(np.load("D:/M8_SNA-135381_17092024_1_g0_imec1/sorted/phy/spike_times.npy"))[:,0]
+ITI = pd.read_csv("D:/M8_SNA-135381_17092024_1_g0_imec1/Meta/ttl_2.csv")
+raw_BPOD = load_mat("D:/M8_SNA-135381_17092024_1_g0_imec1/M8_MA_20240917_181329.mat")
+
 sample_freq = 30000
 
 #%%
 
-ITI = get_ITI_starts(TTL_states, TTL_sample_times, sample_nums,sample_freq)
+ITI = ITI.iloc[:,1]
 
 
 #%%
@@ -43,4 +41,4 @@ EPHYS_trimmed2 = Add_trial(EPHYS_trimmed)
 
 #%%
 
-EPHYS_trimmed = Raw_to_df_all(cluster_group, clust, times, TTL_states, TTL_sample_times, sample_nums, sample_freq, raw_BPOD)
+EPHYS_trimmed2.to_csv("D:/M8_SNA-135381_17092024_1_g0_imec1" + str('/M8_1.csv'))
